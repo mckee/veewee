73a0539b83ccb705d96a8ea2e48aff0016e2c4e9
Brent McKee <brent@tibre.org>
2012-08-24 23:15:36 -0700

    update precise install media url

--- templates/mckee-precise/definition.rb
+++ templates/mckee-precise/definition.rb
@@ -6,7 +6,7 @@ Veewee::Session.declare({
   :hostiocache => 'off',
   :os_type_id => 'Ubuntu_64',
   :iso_file => "ubuntu-12.04-server-amd64.iso",
-  :iso_src => "http://releases.ubuntu.com/12.04/ubuntu-12.04-server-amd64.iso",
+  :iso_src => "http://releases.ubuntu.com/12.04/ubuntu-12.04.1-server-amd64.iso",
   :iso_md5 => 'f2e921788d35bbdf0336d05d228136eb',
   :iso_download_timeout => "1000",
   :boot_wait => "4",
